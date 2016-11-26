2bf9c1685efea8c391e047635088005c17aead1b
Graham J Land <graham@allthingscloud.eu>
2016-11-26 23:29:32 +0000

    Initial Commit

---
+++ k5contractsettingsV1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+global adminUser
+global adminPassword
+global contract
+global contractid
+global defaultid
+global project
+global region
+
+adminUser = 'landg'
+adminPassword = 'happybirthdaytome16'
+contract = 'YssmW1yI'
+contractid = '3256d41b17014d5c99727993d6fca821'
+defaultid = 'eadb882573ac40b1b101eac93009a313'
+project = '3cef1071f8ff432989f18aa14ce3cc66'
+region = 'uk-1'
