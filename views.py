7830292d8a6094938d4d9c1c76b8fe8bebf3405d
Graham J Land <graham@allthingscloud.eu>
2016-11-27 00:44:41 +0000

    templates lnimed

---
+++ views.py
@@ -0,0 +1,24 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'landg'}  # fake user
+    return render_template('hello-flask-login.html',
+                           title='K5 Admin Portal (Beta)',
+                           user=user)
+
+@app.route('/adduser')
+def index():
+    user = {'username': 'landg'}  # fake user
+    return render_template('hello-flask-adduser.html',
+                           title='K5 Add User',
+                           user=user)
+
+@app.route('/userstatus')
+def index():
+    user = {'username': 'landg'}  # fake user
+    return render_template('hello-flask-result.html',
+                           title='K5 User Status',
+                           user=user)
