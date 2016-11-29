783cee1bc39ea348a71175f038a277ffbf9ea30f
Graham J Land <graham@allthingscloud.eu>
2016-11-29 00:06:16 +0000

    Basic Auth Working

--- views.py
+++ views.py
@@ -1,24 +1,37 @@
-from flask import render_template
+from flask import render_template, session, request, redirect, url_for
 from app import app
+import os
 
-@app.route('/')
-@app.route('/login')
+app.secret_key = os.urandom(24)
+
+@app.route('/login', methods=['GET','POST'])
 def index():
-    user = {'username': 'landg'}  # fake user
-    return render_template('hello-flask-login.html',
-                           title='K5 Admin Portal (Beta)',
-                           user=user)
+   if request.method == 'POST':
+     session.pop('user',None)
+
+
+     session['user'] =  request.form['k5username']
+     session['password'] = request.form['k5password']
+     session['contract'] = request.form['k5contract']
+     session['region'] = request.form['k5region']
+
+     if session['password']  == 'password':
+       return redirect(url_for('adduser'))
+     else:
+       return render_template('hello-flask-login.html',
+                           title='K5 Admin Portal (Beta)')
+
+   else:
+     return render_template('hello-flask-login.html',
+                           title='K5 Admin Portal (Beta)')
+
 
 @app.route('/adduser')
 def adduser():
-    user = {'username': 'landg'}  # fake user
     return render_template('hello-flask-adduser.html',
-                           title='K5 Add User',
-                           user=user)
+                           title='K5 Add User')
 
 @app.route('/userstatus')
 def userstatus():
-    user = {'username': 'landg'}  # fake user
     return render_template('hello-flask-result.html',
-                           title='K5 User Status',
-                           user=user)
+                           title='K5 User Status')
